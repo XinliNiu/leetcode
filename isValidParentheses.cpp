d2f48b8340ad6db770327d1d8d6c4188bb36b75f
XinliNiu <niuxinli1989@gmail.com>
2013-09-18 16:23:20 +0800

    Valid Parentheses
    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.

---
+++ isValidParentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+            if(s[i] == ')'){
+                if(st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+            if(s[i] == ']'){
+                if(st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+            }  
+            if(s[i] == '}'){
+                if(st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+            }    
+            
+        }
+        return st.empty();
+    }
+};
